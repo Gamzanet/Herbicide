212004b87c9f53adc02b72f732a35ed289f8de9c
Sori <ljgggg1234@gmail.com>
2024-10-19 13:04:36 +0900

    Running thread, data parsing (#1)
    * dynamic test using thread
    
    * add. minimum test description
    
    * 시작 전 환경변수

--- src/task/tasks.py
+++ src/task/tasks.py
@@ -4,8 +4,8 @@ import time
 from .config import app  # config.py에서 app 객체를 가져옴
 import sys
 import os
-from .parse.dataParse import hookCompareParse, minimumTestParse, getPriceUsingPyth, timeBasedMinimumTestParse
-
+from .parse.dataParse import hookCompareParse, minimumTestParse, getPriceUsingPyth, timeBasedMinimumTestParse, getChkOnlyByPoolManager, timeTestUsingStep
+from .threadWork import threadRun
 # 작업 정의
 @app.task
 def analysis(x, y):
@@ -14,8 +14,6 @@ def analysis(x, y):
     return result.stdout
 @app.task
 def dynamic(timeHash, rpc, currency0, currency1):
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    target_dir = os.path.abspath(os.path.join(current_dir, '..', '..', 'engine', 'gamza-dynamic'))
 
     commands = []
     analysisResult = []
@@ -24,16 +22,29 @@ def dynamic(timeHash, rpc, currency0, currency1):
     print("timeHash : {}".format(timeHash))
     print("rpc : {} , c0 : {} : c1 : {}".format(rpc,currency0, currency1))
     st = time.time()
-    commands.append("forge test --match-path test/inputPoolkey/_MinimumTest.t.sol --rpc-url {}".format(rpc))
-    commands.append("forge test --match-path test/inputPoolkey/_time_std_PoolManager.t.sol  --rpc-url {}".format(rpc))
-    commands.append("forge test --match-path test/hookNoHookCompare/_hookNoHookCompare.t.sol --rpc-url {} -vv | grep using".format(rpc))
-    commands.append("forge test --match-path test/inputPoolkey/_return.t.sol  --rpc-url {} -vv | grep delta-log".format(rpc))
-    
+    option = "--rpc-url {}".format(rpc)
+    _exportPath = "export _targetPoolKey='dynamic_{}.json';".format(timeHash)
+    #head = "forge test --match-path test/inputPoolkey/"
+    #tests = ["_MinimumTest.t.sol","_time_std_PoolManager.t.sol","_hookNoHookCompare.t.sol -vv | grep using","_return.t.sol","_check_onlyByPoolManager.t.sol"]
+    #for _test in tests:
+    #    commands.append("{}{} {}".format(head,_test, option))
+    # commands.append("forge test --match-path test/inputPoolkey/  --rpc-url {}".format(rpc))
+    # commands.append("forge test --match-path test/inputPoolkey/  --rpc-url {}".format(rpc))
+    # commands.append("forge test --match-path test/inputPoolkey/ --rpc-url {} ".format(rpc))
+    # commands.append("forge test --match-path test/inputPoolkey/  --rpc-url {} -vv | grep delta-log".format(rpc))
+    commands.append("{} forge test --match-path test/inputPoolkey/_MinimumTest.t.sol --rpc-url {}".format(_exportPath,rpc))
+    commands.append("{} forge test --match-path test/inputPoolkey/_time_std_PoolManager.t.sol  --rpc-url {}".format(_exportPath,rpc))
+    commands.append("{} forge test --match-path test/inputPoolkey/_hookNoHookCompare.t.sol --rpc-url {} -vv | grep using".format(_exportPath,rpc))
+    commands.append("{} forge test --match-path test/inputPoolkey/_return.t.sol  --rpc-url {} -vv | grep delta-log".format(_exportPath,rpc))
+    commands.append("{} forge test --match-path test/inputPoolkey/_check_onlyByPoolManager.t.sol --rpc-url {}".format(_exportPath,rpc))
+    commands.append("{} forge test --match-path test/inputPoolkey/_time_minimum_step.t.sol --rpc-url {} -vvv".format(_exportPath,rpc))
+    threads = []
     for command in commands:
-        print(command)
-        path = os.path.abspath(__file__)
-        print(path)
-        result = subprocess.run(command, shell=True, capture_output=True, text=True, cwd=target_dir)
+        threads.append(threadRun(command))
+
+    analysisResult = []
+    for thread in threads:
+        result = thread.join()
         analysisResult.append(result)
     ed = time.time()
     print("Done : {}".format(ed))
@@ -46,6 +57,8 @@ def dynamic(timeHash, rpc, currency0, currency1):
     analysisResult[2] = hookCompareParse(analysisResult[2])
     print("end 2")
     analysisResult[3] = getPriceUsingPyth(rpc, currency0, currency1, analysisResult[3])#analysisResult[0]))
+    analysisResult[4] = getChkOnlyByPoolManager(analysisResult[4])
+    analysisResult[5] = timeTestUsingStep(analysisResult[5])
     ed = time.time()
     print("Done : {}".format(ed))
     print("time : {}".format(ed - st))
--- src/task/threadWork.py
+++ src/task/threadWork.py
@@ -1,30 +1,27 @@
 import threading
-
-
-
+import os
+import subprocess
 class ThreadWithReturnValue(threading.Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None):
         threading.Thread.__init__(self, group, target, name, args, kwargs)
         self._return = None
-    
     def run(self):
         if self._target is not None:
             self._return = self._target(*self._args, **self._kwargs)
-    
     def join(self, *args):
         threading.Thread.join(self, *args)
         return self._return
 
-def f(x):
-    return 1
-
-
-thread = ThreadWithReturnValue(target=f, args=(5))
-
-
-thread.start()
-
-
-result = thread.join()
-
-print(f"결과: {result}")
+def testRun(command):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    target_dir = os.path.abspath(os.path.join(current_dir, '..', '..', 'engine', 'gamza-dynamic'))
+    print(command)
+    path = os.path.abspath(__file__)
+    print(path)
+    result = subprocess.run(command, shell=True, capture_output=True, text=True, cwd=target_dir)
+    return result
+def threadRun(command):
+    thread = ThreadWithReturnValue(target=testRun, args=(command,))
+    thread.start()
+    return thread
+    
